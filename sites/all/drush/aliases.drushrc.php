50be72d975c49586f7be5d3d03e1eef88a275dac
Matt Elliott <melliott@ccistudios.com>
2012-08-20 10:04:12 -0400

    stuff

---
+++ sites/all/drush/aliases.drushrc.php
@@ -0,0 +1,8 @@
+<?php
+
+$aliases['dev'] = array(
+		'uri'					=> 'dev.devinedetails.ca',
+		'root' 				=> '/home/divine/subdomains/dev/public_html',
+		'remote-host' => 'divinedetails.ca',
+		'remote-user' => 'divine',
+);
