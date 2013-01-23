818d0ccc9898e4470d5454daffba11e205b5f4de
Jonathan Bennett <jbennett@ccistudios.com>
2013-01-23 15:14:37 -0500

    Added live alias

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -1,8 +1,15 @@
 <?php
 
 $aliases['dev'] = array(
-		'uri'					=> 'dev.devinedetails.ca',
-		'root' 				=> '/home/divine/subdomains/dev/public_html',
-		'remote-host' => 'divinedetails.ca',
-		'remote-user' => 'divine',
+	'uri'			=> 'dev.devinedetails.ca',
+	'root' 			=> '/home/divine/subdomains/dev/public_html',
+	'remote-host'	=> 'divinedetails.ca',
+	'remote-user'	=> 'divine',
+);
+
+$aliases['live'] = array(
+	'uri'			=> 'devinedetails.ca',
+	'root' 			=> '/home/divine/subdomains/live/public_html',
+	'remote-host' 	=> 'divinedetails.ca',
+	'remote-user' 	=> 'divine',
 );
